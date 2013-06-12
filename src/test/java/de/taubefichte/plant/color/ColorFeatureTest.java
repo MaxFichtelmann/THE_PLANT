cb25e5063d57f2a7704b134f1ff35b44cb0fa6a0
Max Fichtelmann <max.fichtelmann@gmail.com>
2013-06-12 22:10:08 +0200

    setup implementations for ColorFeature tests

--- src/test/java/de/taubefichte/plant/color/ColorFeatureTest.java
+++ src/test/java/de/taubefichte/plant/color/ColorFeatureTest.java
@@ -1,9 +1,10 @@
 package de.taubefichte.plant.color;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
+import static de.taubefichte.plant.Dominance.*;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ColorFeatureTest
@@ -24,22 +25,42 @@ public class ColorFeatureTest
     ColorFeature blueDominant;
     
     /**
-     * gene1: standard, red
-     * gene2: standard, white
+     * gene1: standard, red gene2: standard, white
      */
     ColorFeature pinkStandardMix;
     
     /**
-     * gene1: standard, red
-     * gene2: recessive, yellow
+     * gene1: standard, red gene2: recessive, yellow
      */
     ColorFeature redStandard;
     
     /**
-     * gene1: recessive, red
-     * gene2: recessive, yellow
+     * gene1: recessive, red gene2: recessive, yellow
      */
     ColorFeature orangeRecessiveMix;
+    
+    @Before
+    public void setup()
+    {
+        ColorAttribute red = new ColorAttributeImplementation( new Color( 255, 0, 0 ) );
+        ColorAttribute yellow = new ColorAttributeImplementation( new Color( 255, 255, 0 ) );
+        ColorAttribute blue = new ColorAttributeImplementation( new Color( 0, 0, 255 ) );
+        ColorAttribute white = new ColorAttributeImplementation( new Color( 255, 255, 255 ) );
+        
+        ColorGeneImplementation redDominant = new ColorGeneImplementation( DOMINANT, red );
+        ColorGeneImplementation redStandard = new ColorGeneImplementation( STANDARD, red );
+        ColorGeneImplementation redRecessive = new ColorGeneImplementation( RECESSIVE, red );
+        ColorGeneImplementation yellowRecessive = new ColorGeneImplementation( RECESSIVE, yellow );
+        ColorGeneImplementation blueDominant = new ColorGeneImplementation( DOMINANT, blue );
+        ColorGeneImplementation whiteStandard = new ColorGeneImplementation( STANDARD, white );
+        
+        this.pureRed = new ColorFeatureImplementation( redDominant, redDominant );
+        this.redDominant = new ColorFeatureImplementation( redDominant, yellowRecessive );
+        this.blueDominant = new ColorFeatureImplementation( blueDominant, redStandard );
+        this.pinkStandardMix = new ColorFeatureImplementation( redStandard, whiteStandard );
+        this.redStandard = new ColorFeatureImplementation( redStandard, yellowRecessive );
+        this.orangeRecessiveMix = new ColorFeatureImplementation( redRecessive, yellowRecessive );
+    }
     
     @Test
     public void pure()
